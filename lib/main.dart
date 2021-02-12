644ed5009a1a51510a520128a38a918eb6472033
JM <ad4674@gmail.com>
2021-02-12 16:04:54 +0900

    Stack 예제 Frame 레이아웃처럼 겹치게 사용 가능 (ex 화면위의 로딩, 이미지위에 텍스트)

--- lib/main.dart
+++ lib/main.dart
@@ -38,29 +38,26 @@ class MyHomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text("제목"),
       ),
-      body: Row(
-        mainAxisSize: MainAxisSize.max,
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
+      body: Stack(
         children: <Widget>[
           Container(
             color: Colors.red,
-            width: 110,
+            width: 100,
             height: 100,
             padding: const EdgeInsets.all(8.0),
             margin: const EdgeInsets.all(8.0),
           ),
           Container(
             color: Colors.green,
-            width: 100,
-            height: 100,
+            width: 80,
+            height: 80,
             padding: const EdgeInsets.all(8.0),
             margin: const EdgeInsets.all(8.0),
           ),
           Container(
             color: Colors.blue,
-            width: 100,
-            height: 100,
+            width: 60,
+            height: 60,
             padding: const EdgeInsets.all(8.0),
             margin: const EdgeInsets.all(8.0),
           ),
