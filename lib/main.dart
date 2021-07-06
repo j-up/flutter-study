10087f372a6d7572c9f2410b0483265099efdf77
JM <ad4674@gmail.com>
2021-07-06 23:44:20 +0900

    카운팅 앱 예제 따라하기

--- lib/main.dart
+++ lib/main.dart
@@ -4,51 +4,58 @@ void main() {
   runApp(MyApp());
 }
 
+// 정적(바뀌지않음)
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(),
+      title:'Demos',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: MyHomePage(title: 'Flutter Demo Home Page')
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
+// 동적(바뀜)
+class MyHomePage extends StatefulWidget {
+  final String title;
+
+  MyHomePage({Key key, this.title}) : super(key: key);
+  @override
+  _State createState() => _State();
+}
+
+class _State extends State<MyHomePage> {
+  // _ = private 변수
+  int _counter = 0;
+
   @override
   Widget build(BuildContext context) {
-    final items = List.generate(100, (index) => index).toList();
     return Scaffold(
-      appBar: AppBar(
-        title: Text("제목"),
-      ),
-      /*
-      Tab과 연동하여 사용하지않으면 좌우로 슬라이드가 가능한지 모를수 있기에 단독으로는 사용하지않음
-       */
-      body: PageView(
+      appBar: AppBar(title: Text(widget.title),),
+      body: Center(
+        // 칼럼은 리스트뷰처럼 한줄 한줄 생김
+        child: Column(mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
-          Container(color: Colors.red),
-          Container(color: Colors.blue),
-          Container(color: Colors.pink),
+          Text('many times:'),
+          Text('$_counter', style: Theme.of(context).textTheme.display1)
+
         ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter(),
+        tooltip: 'increment',
+        child: Icon(Icons.add)
       ),
     );
   }
+
+  _incrementCounter() {
+    // 바뀐사항 적용
+    setState(() {
+      _counter++;
+    });
+  }
 }
+
