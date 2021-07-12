ed677ce828df38d8e338db657e32a6532b63be78
JM <ad4674@gmail.com>
2021-07-12 19:24:49 +0900

    appbar 타이틀과 간단한 사진출력 예제

--- lib/main.dart
+++ lib/main.dart
@@ -1,60 +1,18 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
+  runApp(MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: SafeArea(
-          child: Column(
-            children: <Widget>[
-              CircleAvatar(
-                radius: 50,
-                backgroundImage: AssetImage('images/test.jpg'),
-              ),
-              Text(
-                "JM",
-                style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
-              ),
-              Text(
-                "FLUTTER DEVELOPER",
-                style: TextStyle(
-                  fontSize: 20.0,
-                  color: Colors.teal,
-                  letterSpacing: 2.5,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              Card(
-                  color: Colors.white,
-                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
-                  child: Padding(
-                    padding: EdgeInsets.all(18),
-                    child: Row(
-                      children: <Widget>[
-                        Icon(
-                          Icons.phone,
-                          color: Colors.green,
-                        ),
-                        SizedBox(
-                          width: 10,
-                        ),
-                        Text(
-                          "010-1234-5678",
-                          style: TextStyle(color: Colors.black, fontSize: 20.0),
-                        )
-                      ],
-                    ),
-                  ))
-            ],
+        appBar: AppBar(
+          title: Text("Scaffold example"),
+          backgroundColor: Colors.blueGrey,
+        ),
+        body: Padding(
+          padding: EdgeInsets.all(10),
+          child: Image(
+            image: NetworkImage("https://images.pexels.com/photos/67636/rose-blue-flower-rose-blooms-67636.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"),
           ),
         ),
       ),
-    );
-  }
+  ));
 }
