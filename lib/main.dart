e28da35af0e84ea24a5afb95cc38f965954a628c
JM <ad4674@gmail.com>
2021-07-12 19:36:51 +0900

    Column & Row 예제

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,44 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text("Scaffold example"),
-          backgroundColor: Colors.blueGrey,
-        ),
-        body: Padding(
-          padding: EdgeInsets.all(10),
-          child: Image(
-            image: NetworkImage("https://images.pexels.com/photos/67636/rose-blue-flower-rose-blooms-67636.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"),
-          ),
+        backgroundColor: Colors.teal,
+        // notch 바로 아래 영역에 표시 (안전한 영역)
+        body: SafeArea(
+          // Column -> 세로 1줄씩 세로 3줄
+          // Row -> 가로로 1개씩 가로 1줄에 3개
+            child: Column(
+              // 복수의 자식들이 있어야하기에 children 속성 사용
+              children: <Widget>[
+                Container(
+                  height: 100.0,
+                  width: 100.0,
+                  color: Colors.white,
+                  child: Text('hello'),
+                ),
+                Container(
+                  height: 100.0,
+                  width: 100.0,
+                  color: Colors.blue,
+                  child: Text('hello'),
+                ),
+                Container(
+                  height: 100.0,
+                  width: 100.0,
+                  color: Colors.red,
+                  child: Text('hello'),
+                ),
+              ],
+            ),
         ),
       ),
-  ));
+    );
+  }
 }
